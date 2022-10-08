b125e55499c026a81eec625e7970973101eb424c
Roman Okatev <nebulawalker@yandex.ru>
2022-10-08 21:45:20 +0300

    Первый коммит. Шаг 9 курса.

---
+++ datetime_helpers.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+COMPANY_FOUNDATION_YEAR = 1920
+def company_age():
+    current_year = datetime.datetime.now().year
+    delta = current_year - COMPANY_FOUNDATION_YEAR
+
+    if delta % 100 >= 11 and delta % 100 <= 14:
+        ending = "лет"
+    elif delta % 10 == 1:
+        ending = "год"
+    elif delta % 10 in (2, 3, 4):
+        ending = "года"
+    else:
+        ending = "лет"
+    result = f"{delta} {ending}"
+    return result
